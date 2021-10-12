a25af7e42530deb873af463d0ea23cb94fa91a6b
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2021-10-13 00:27:22 +0200

    Añadidos los prints

--- src/LaVida/Noviva.java
+++ src/LaVida/Noviva.java
@@ -4,5 +4,5 @@ public class Noviva {
 
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("No siempre se gana.");
 }
--- src/LaVida2/Viva.java
+++ src/LaVida2/Viva.java
@@ -2,5 +2,5 @@ package LaVida2;
 
 public class Viva {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("La vida es bella supuestamente.");
 }
